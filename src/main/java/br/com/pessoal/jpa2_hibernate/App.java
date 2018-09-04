e6bf329e00b64b693ba0e1f5240890604bb4abe3
mupezzuol <murillopezzuol@hotmail.com>
2018-09-04 16:33:12 -0300

    Projeto maven criado.

---
+++ src/main/java/br/com/pessoal/jpa2_hibernate/App.java
@@ -0,0 +1,13 @@
+package br.com.pessoal.jpa2_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
